145059a314307726873401dc7d089b7097c9b3d2
adityamo <rfadityahalim18@gmail.com>
2023-01-01 10:00:42 +0700

    custom button music

---
+++ src/components/FloatingMusic/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="">
+    <h3 class="">test</h3>
+  </div>
+</template>
--- src/pages/mobile-dialog/index.vue
+++ src/pages/mobile-dialog/index.vue
@@ -1,23 +1,27 @@
 <template>
-  <ChatDialog :options="chatOptions"
-              :title="props.title"
-              @msg-click="handleMsgClick"></ChatDialog>
+  <!-- <FloatingMusic /> -->
+  <ChatDialog
+    :options="chatOptions"
+    :title="props.title"
+    @msg-click="handleMsgClick"
+  ></ChatDialog>
 </template>
 
 <script setup>
-import { ref, defineProps, onMounted } from 'vue'
-import ChatDialog from '../../components/chat-dialog/index.vue'
+import { ref, defineProps, onMounted } from 'vue';
+import ChatDialog from '../../components/chat-dialog/index.vue';
+import FloatingMusic from '../../components/FloatingMusic/index.vue';
 const props = defineProps({
   src: String,
-  title: String
+  title: String,
 });
 const chatOptions = ref();
 onMounted(() => {
   $.getJSON(props.src, (options) => {
     chatOptions.value = options;
   });
-})
-function handleMsgClick ({ author, content, type }) {
-  console.log(author, content, type)
+});
+function handleMsgClick({ author, content, type }) {
+  console.log(author, content, type);
 }
 </script>
