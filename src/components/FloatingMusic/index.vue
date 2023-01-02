39b4a06125907c81d37445ecdaec4562db61a87c
adityamo <rfadityahalim18@gmail.com>
2023-01-02 21:14:16 +0700

    percobaan1

--- src/components/FloatingMusic/index.vue
+++ src/components/FloatingMusic/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="">
     <audio
-      id="myAudio"
       src="../../../public/assets/music/Prep.mp3"
+      id="myAudio"
+      ref="audio"
       class="hide"
       allow="autoplay"
       autoplay="true"
+      type="audio/mpeg"
       loop
     ></audio>
     <img
@@ -14,36 +16,65 @@
       alt=""
       id="icon-music"
       class=""
-      @click="toggleMusic()"
+      v-on:click="toggleMusic"
     />
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import './css/main.scss';
+import { defineComponent, onMounted, ref } from 'vue';
 
-export default {
+export default defineComponent({
+  name: 'CommunicateVoice',
   data() {
     return {
       isPlaying: false,
       myAudio: document.getElementById('myAudio'),
     };
   },
+  setup() {
+    const audio = ref<HTMLAudioElement>();
+    onMounted(() => {
+      console.log(audio);
+      //@ts-ignore
+      audio.value?.play();
+    });
+    return {
+      audio,
+    };
+  },
   methods: {
     toggleMusic() {
-      let myAudio = document.getElementById('myAudio');
-      if (this.isPlaying == false) {
-        myAudio.play();
-        this.isPlaying = true;
+      this.isPlaying = !this.isPlaying;
+      if (this.isPlaying) {
+        this.audio.play();
       } else {
-        myAudio.pause();
-        this.isPlaying = false;
+        this.audio.pause();
       }
-      console.log(this.isPlaying);
     },
   },
-  // mounted() {
-  //   this.$refs.music.click();
-  // },
-};
+});
+// yang bisa
+// export default {
+//   data() {
+//     return {
+//       isPlaying: false,
+//       myAudio: document.getElementById('myAudio'),
+//     };
+//   },
+//   methods: {
+//     toggleMusic() {
+//       let myAudio = document.getElementById('myAudio');
+//       if (this.isPlaying == false) {
+//         myAudio.play();
+//         this.isPlaying = true;
+//       } else {
+//         myAudio.pause();
+//         this.isPlaying = false;
+//       }
+//       console.log(this.isPlaying);
+//     },
+//   },
+// };
 </script>
